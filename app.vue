f4365eb7308fdd54b8372b51dee5f6d16933b8c5
P3RK STUDIOS <parth.kaushal.007@gmail.com>
2022-05-08 13:54:34 +0530

    v1

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-alert>Hare Krishna {{version}}</v-alert>
+  <v-btn @click="login()">Login</v-btn>
+  <v-btn @click="logOut()">Logout</v-btn>
+</template>
+
+<script setup>
+import Moralis from 'Moralis'
+// connect to Moralis server
+
+const serverUrl = "https://bmqo8d2tvbzr.usemoralis.com:2053/server";
+const appId = "BiO0g3AGggsWXVvvTWpCamBpMnP5tNHNo4DaApUo";
+Moralis.start({ serverUrl, appId });
+
+// add from here down
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate();
+  }
+  console.log("logged in user:", user);
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+
+const version = 2 + 1
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    ssr: false,
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+      transpile: ['vuetify'],
+    },
+    vite: {
+      define: {
+        'process.env.DEBUG': false,
+      },
+    },    
+})
